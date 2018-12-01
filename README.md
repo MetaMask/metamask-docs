703c855243525b7b50fb32285cad7c83c584639e
Paul Bouchon <mail@bitpshr.net>
2018-11-30 22:40:59 -0500

    Project inception

---
+++ README.md
@@ -0,0 +1,5 @@
+MetaMask project documentation
+
+## Usage
+
+Documentation is auto-deployed when markdown files on the `master` branch are edited. No installation is necessary.
---
+++ uncrate.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    description: 'MetaMask project documentation',
+    logo: 'https://metamask.io/img/metamask.png',
+    name: 'MetaMask',
+    repo: 'metamask/metamask-extension'
+};
