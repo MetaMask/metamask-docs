238c770e488cdcf068b881480d8bd08b32501de6
bitpshr <mail@bitpshr.net>
2019-01-02 12:16:58 -0500

    Update logo

--- uncrate.config.js
+++ uncrate.config.js
@@ -1,8 +1,7 @@
 module.exports = {
     description: 'MetaMask project documentation',
     excludes: ['README', '.git'],
-    logo: 'https://metamask.io/img/metamask.png',
-    name: 'MetaMask',
+    logo: 'https://i.ibb.co/QkbsTmJ/Screen-Shot-2019-01-02-at-12-12-08-PM.png',
     repo: 'metamask/metamask-extension',
     sourceRepo: 'metamask/metamask-docs',
     root: '/metamask-docs/',
