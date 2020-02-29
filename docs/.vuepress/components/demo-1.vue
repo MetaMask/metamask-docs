36d29e6842428aa324818d2c6e4fd0d03c4a632b
unknown <austin.akers5@gmail.com>
2020-02-29 09:58:51 -0600

    added scraps

--- docs/.vuepress/components/demo-1.vue
+++ docs/.vuepress/components/demo-1.vue
@@ -1,14 +1,22 @@
 <template>
   <p class="demo">
     {{ msg }}
+    {{testingScraps()}}
   </p>
 </template>
 
 <script>
+import { Scraps } from '../../../src/js/helpers/scraps'
 export default {
   data () {
     return {
       msg: 'Hello this is <demo-1>'
+    }
+  },
+  methods: {
+    testingScraps() {
+      const scraps = new Scraps();
+      console.log(scraps.executeStack(false));
     }
   }
 }
