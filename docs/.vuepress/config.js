a5750affe0e9388ebefb1cf4b078db587d2fe101
Erik Marks <rekmarks@protonmail.com>
2020-09-01 09:18:02 -0700

    Edit description

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,7 +5,7 @@ module.exports = (_ctx) => ({
     '/': {
       lang: 'en-US',
       title: 'MetaMask Docs',
-      description: 'Developer documentation for the popular Ethereum wallet.'
+      description: 'Developer documentation for the MetaMask Ethereum wallet'
     }
   },
 
