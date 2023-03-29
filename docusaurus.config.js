352b0b71acfdd5fc13073332b965f35f86f120b8
Alexandra Tran Carrillo <12214231+alexandratran@users.noreply.github.com>
2023-03-29 10:14:58 -0700

    [minor] fix footer link (#81)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -111,7 +111,7 @@ const config = {
               },
               {
                 label: "Tutorials",
-                to: "/wallet/category/tutorials",
+                to: "/wallet/tutorials",
               },
               {
                 label: "Reference",
