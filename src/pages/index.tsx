c1e2099c25b604966e60ca902af717b5e2ff6fb7
Shane <jonas.shane@gmail.com>
2023-03-30 09:52:32 -0700

    Fixed homepage redirect (#88)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { Redirect } from "@docusaurus/router";
+import useBaseUrl from "@docusaurus/useBaseUrl";
 
 export default function Home(): JSX.Element {
   return (
-    <Redirect to="/wallet" />
+    <Redirect to={useBaseUrl("/wallet")} />
   );
 }
