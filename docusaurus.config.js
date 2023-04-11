44624650efde5b95851ef217d6c18615609bb2de
Shane <jonas.shane@gmail.com>
2023-04-11 16:07:56 -0700

    Added algolia with public api keys (#77)
    * added algolia config in theme
    
    * Added public api keys for algolia
    
    * Changed docasaurus version to 2.4 and added replaceSearchResultPathName for local development
    
    * Added env variable to config branch path
    
    * Added support for preview branches via process.env.DEST
    
    * Fixed yarn lock
    
    * Fixed algolia settings
    
    * Update docusaurus.config.js
    
    ---------
    
    Co-authored-by: Austin Akers <austin.akers5@gmail.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -321,6 +321,35 @@ const config = {
         theme: codeTheme,
         additionalLanguages: ["csharp"],
       },
+      algolia: {
+        // The application ID provided by Algolia
+        appId: "AWX4QVM59R",
+
+        // Public API key: it is safe to commit it
+        apiKey: "6095a25a6824bfa909fa0692e6847ec4",
+
+        indexName: "mm--v2-staging",
+
+        // Optional: see doc section below
+        contextualSearch: true,
+
+        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        // externalUrlRegex: "external\\.com|domain\\.com",
+
+        // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+        replaceSearchResultPathname: {
+          from:  "/",
+          to: process.env.DEST || "/",
+        },
+
+        // Optional: Algolia search parameters
+        searchParameters: {},
+
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        searchPagePath: "search",
+
+        //... other Algolia params
+      },
     }),
 };
 
