c89164059a61e12bb6b8f6683b3220c80d6dcf5b
alexandratran <alexandratran@users.noreply.github.com>
2023-04-24 23:36:17 +0000

    deploy: 89e10f869e4eceae6b0f3efff917ea98d6c3d556

---
+++ assets/js/216cb7de.180c8af9.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmetamask_docs=self.webpackChunkmetamask_docs||[]).push([[6322],{355:e=>{e.exports=JSON.parse('{"title":"Concepts","slug":"/category/concepts","permalink":"/snaps/category/concepts","navigation":{"previous":{"title":"Troubleshoot","permalink":"/snaps/how-to/troubleshoot"},"next":{"title":"Snap anatomy","permalink":"/snaps/concepts/anatomy"}}}')}}]);
