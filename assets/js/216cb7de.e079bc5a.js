09e99ef95916a2e178a6327489bdca3489a1234a
shanejonas <shanejonas@users.noreply.github.com>
2023-05-04 16:32:29 +0000

    deploy: 5fa2d7e0d73245f04de3cf0fe8d93ad5016427e9

---
+++ assets/js/216cb7de.e079bc5a.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmetamask_docs=self.webpackChunkmetamask_docs||[]).push([[6322],{355:s=>{s.exports=JSON.parse('{"title":"Concepts","slug":"/category/concepts","permalink":"/snaps/category/concepts","navigation":{"previous":{"title":"Troubleshoot","permalink":"/snaps/how-to/troubleshoot"},"next":{"title":"Snaps anatomy","permalink":"/snaps/concepts/anatomy"}}}')}}]);
