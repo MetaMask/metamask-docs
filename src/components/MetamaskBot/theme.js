231af682ec96f469eac4bc1681bd367469bfaf56
dms120 <david.sampaio@consensys.net>
2023-05-14 22:12:26 +0100

    fix: Linting

--- src/components/MetamaskBot/theme.js
+++ src/components/MetamaskBot/theme.js
@@ -5,15 +5,15 @@ export const theme = createTheme({
     type: "light",
     primary: {
       main: "rgb(3, 125, 214)", // Blue
-      light: 'rgb(171, 217, 229)',
-      dark: 'rgb(105, 145, 156)',
-      contrastText: 'rgba(0, 0, 0, 0.87)'
+      light: "rgb(171, 217, 229)",
+      dark: "rgb(105, 145, 156)",
+      contrastText: "rgba(0, 0, 0, 0.87)",
     },
     secondary: {
       main: "#5c6bc0", // Orange
-      light: 'rgb(124, 136, 204)',
-      dark: 'rgb(64, 74, 134)',
-      contrastText: '#fff'
+      light: "rgb(124, 136, 204)",
+      dark: "rgb(64, 74, 134)",
+      contrastText: "#fff",
     },
   },
 });
