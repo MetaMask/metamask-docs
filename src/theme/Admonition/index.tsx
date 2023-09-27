8f910e744fc1c37d8ee0c0feda0a47e219729765
Christian Montoya <christian.montoya@consensys.net>
2023-09-26 21:29:30 -0400

    yarn lint fix

--- src/theme/Admonition/index.tsx
+++ src/theme/Admonition/index.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import Admonition from '@theme-original/Admonition';
-import { Props as AdmonitionPropsRaw } from '@theme/Admonition';
-import type {WrapperProps} from '@docusaurus/types';
+import React from "react";
+import Admonition from "@theme-original/Admonition";
+import { Props as AdmonitionPropsRaw } from "@theme/Admonition";
+import type { WrapperProps } from "@docusaurus/types";
 
-import FlaskIcon from './flask-icon.svg';
-import './index.css';
+import FlaskIcon from "./flask-icon.svg";
+import "./index.css";
 
-type AdmonitionProps = Omit<AdmonitionPropsRaw, 'type'> & {
-  type: AdmonitionPropsRaw['type'] | 'flaskOnly';
+type AdmonitionProps = Omit<AdmonitionPropsRaw, "type"> & {
+  type: AdmonitionPropsRaw["type"] | "flaskOnly";
 }
 
 declare function AdmonitionComponent(props: AdmonitionProps): JSX.Element;
@@ -15,7 +15,7 @@ declare function AdmonitionComponent(props: AdmonitionProps): JSX.Element;
 type Props = WrapperProps<typeof AdmonitionComponent>;
 
 export default function AdmonitionWrapper(props: Props): JSX.Element {
-  if (props.type === 'flaskOnly') {
+  if (props.type === "flaskOnly") {
     return (
       <Admonition
         {...props}
--- src/theme/DocSidebarItem/index.tsx
+++ src/theme/DocSidebarItem/index.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import DocSidebarItem from '@theme-original/DocSidebarItem';
-import type DocSidebarItemType from '@theme/DocSidebarItem';
-import type {WrapperProps} from '@docusaurus/types';
-import clsx from 'clsx';
+import React from "react";
+import DocSidebarItem from "@theme-original/DocSidebarItem";
+import type DocSidebarItemType from "@theme/DocSidebarItem";
+import type { WrapperProps } from "@docusaurus/types";
+import clsx from "clsx";
 
-import styles from './index.module.css';
+import styles from "./index.module.css";
 
 type Props = WrapperProps<typeof DocSidebarItemType>;
 
 export default function DocSidebarItemWrapper(props: Props): JSX.Element {
-  if (props.item.type === 'category' || props.item.type === 'link') {
+  if (props.item.type === "category" || props.item.type === "link") {
     const { className, ...itemProps } = props.item;
 
     return (
@@ -21,13 +21,13 @@ export default function DocSidebarItemWrapper(props: Props): JSX.Element {
             className: clsx(
               className,
               {
-                [styles.flaskOnly]: itemProps.customProps?.flask_only
-              }
+                [styles.flaskOnly]: itemProps.customProps?.flask_only,
+              },
             ),
           }}
         />
       </>
-    )
+    );
   }
   return (
     <>
