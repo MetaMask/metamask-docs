bd8919eee6bfa7e92588990c37ce4edcf5d9c5ea
Alexandra Tran <alexandratran@protonmail.com>
2023-10-19 15:04:02 -0700

    class -> className

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -38,7 +38,7 @@ function DevBanner() {
         </p>
         <Link className={clsx(styles.devBannerButton, "button button--lg")} href="https://github.com/MetaMask/metamask-extension/blob/develop/docs/README.md">
           Contribute
-          <svg width="15" height="15" aria-hidden="true" viewBox="0 0 24 24" class="iconExternalLink_node_modules-@docusaurus-theme-classic-lib-theme-Icon-ExternalLink-styles-module"><path fill="currentColor" d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z"></path></svg>
+          <svg width="15" height="15" aria-hidden="true" viewBox="0 0 24 24" className="iconExternalLink_node_modules-@docusaurus-theme-classic-lib-theme-Icon-ExternalLink-styles-module"><path fill="currentColor" d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z"></path></svg>
         </Link>
       </div>
     </header>
