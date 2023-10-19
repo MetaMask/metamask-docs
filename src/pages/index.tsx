4edf40b8bb2f8507c1fe5439b7976a2ce8c42091
Alexandra Tran <alexandratran@protonmail.com>
2023-10-19 15:58:20 -0700

    add space

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -37,7 +37,7 @@ function DevBanner() {
           Join the MetaMask developer community and learn how to contribute to the MetaMask project itself.
         </p>
         <Link className={clsx(styles.devBannerButton, "button button--lg")} href="https://github.com/MetaMask/metamask-extension/blob/develop/docs/README.md">
-          Contribute
+          Contribute&nbsp;
           <svg width="15" height="15" aria-hidden="true" viewBox="0 0 24 24" className="iconExternalLink_node_modules-@docusaurus-theme-classic-lib-theme-Icon-ExternalLink-styles-module"><path fill="currentColor" d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z"></path></svg>
         </Link>
       </div>
