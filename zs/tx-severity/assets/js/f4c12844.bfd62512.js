eab2f27c1bd500ef403f74d231b0d6f7898439af
ziad-saab <ziad-saab@users.noreply.github.com>
2023-11-17 12:34:13 +0000

    deploy: 969f5109f46a6b19819345eefc28d35de35cd594

---
+++ zs/tx-severity/assets/js/f4c12844.bfd62512.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmetamask_docs=self.webpackChunkmetamask_docs||[]).push([[8906],{18904:e=>{e.exports=JSON.parse('{"title":"JSON-RPC API playground","slug":"wallet/reference/json-rpc-api","permalink":"/zs/tx-severity/wallet/reference/json-rpc-api","navigation":{"previous":{"title":"JSON-RPC API","permalink":"/zs/tx-severity/wallet/reference/rpc-api"}}}')}}]);
