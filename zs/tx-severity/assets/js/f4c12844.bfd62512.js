47f4060c66c8ef2725ce30e3032baef6b9a1504b
Montoya <Montoya@users.noreply.github.com>
2023-11-21 02:35:59 +0000

    deploy: eb1f140893d08cd3ccefbe08838bf8879ab2eb3a

--- zs/tx-severity/assets/js/f4c12844.bfd62512.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmetamask_docs=self.webpackChunkmetamask_docs||[]).push([[8906],{18904:e=>{e.exports=JSON.parse('{"title":"JSON-RPC API playground","slug":"wallet/reference/json-rpc-api","permalink":"/zs/tx-severity/wallet/reference/json-rpc-api","navigation":{"previous":{"title":"JSON-RPC API","permalink":"/zs/tx-severity/wallet/reference/rpc-api"}}}')}}]);
