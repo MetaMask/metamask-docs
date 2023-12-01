37945542222a1d83c6de1830b016e8134bfccbd9
alexandratran <alexandratran@users.noreply.github.com>
2023-12-01 03:29:16 +0000

    deploy: 1d1a772e09619f48307024998cae07fd69ccee2b

---
+++ 1027-truffle/assets/js/5577556d.e2e23334.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmetamask_docs=self.webpackChunkmetamask_docs||[]).push([[8227],{6178:e=>{e.exports=JSON.parse('{"title":"JSON-RPC API","slug":"wallet/reference/json-rpc-api","permalink":"/1027-truffle/wallet/reference/json-rpc-api","navigation":{"previous":{"title":"Ethereum provider API","permalink":"/1027-truffle/wallet/reference/provider-api"}}}')}}]);
