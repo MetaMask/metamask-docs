1c41fa82ba9b4521d8b989e8e6d6551667ed0f0b
Montoya <Montoya@users.noreply.github.com>
2023-12-21 14:40:22 +0000

    deploy: 6acfdc39e27fbfa768e92d98659c2f50310a9b57

--- 844-resources/assets/js/d2d7e50a.6e1d7dde.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmetamask_docs=self.webpackChunkmetamask_docs||[]).push([[9700],{69852:e=>{e.exports=JSON.parse('{"title":"JSON-RPC API","slug":"wallet/reference/json-rpc-api","permalink":"/844-resources/wallet/reference/json-rpc-api","navigation":{"previous":{"title":"Ethereum provider API","permalink":"/844-resources/wallet/reference/provider-api"}}}')}}]);
