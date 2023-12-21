21dcb5969d83961697afa31399f14df0b375b5e1
Montoya <Montoya@users.noreply.github.com>
2023-12-21 14:38:26 +0000

    deploy: eee907ec01bee7a87a82142d2c370bdbbd93828a

--- 983-snaps-accessible-design/assets/js/2150471b.63f92c90.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmetamask_docs=self.webpackChunkmetamask_docs||[]).push([[4957],{7085:e=>{e.exports=JSON.parse('{"name":"docusaurus-theme-search-algolia","id":"default"}')}}]);
