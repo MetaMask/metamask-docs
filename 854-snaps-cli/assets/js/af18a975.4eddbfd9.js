d96e078e465335a8e3686c0ac9cc08062d798c63
alexandratran <alexandratran@users.noreply.github.com>
2024-01-04 21:20:45 +0000

    deploy: a764d1b30cf9b50370a598031e68e86893cf0b34

--- 854-snaps-cli/assets/js/af18a975.4eddbfd9.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmetamask_docs=self.webpackChunkmetamask_docs||[]).push([[2227],{5065:e=>{e.exports=JSON.parse('{"title":"JSON-RPC API","slug":"wallet/reference/json-rpc-api","permalink":"/854-snaps-cli/wallet/reference/json-rpc-api","navigation":{"previous":{"title":"Ethereum provider API","permalink":"/854-snaps-cli/wallet/reference/provider-api"}}}')}}]);
