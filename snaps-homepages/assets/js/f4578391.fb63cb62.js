a8dc942870a37ccd084d5078f748a93b2db61261
Montoya <Montoya@users.noreply.github.com>
2024-01-05 23:42:43 +0000

    deploy: 8a097d7e17bd585c43199bee9a06efc356449825

--- snaps-homepages/assets/js/f4578391.fb63cb62.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmetamask_docs=self.webpackChunkmetamask_docs||[]).push([[1910],{54289:e=>{e.exports=JSON.parse('{"title":"JSON-RPC API","slug":"wallet/reference/json-rpc-api","permalink":"/snaps-homepages/wallet/reference/json-rpc-api","navigation":{"previous":{"title":"Ethereum provider API","permalink":"/snaps-homepages/wallet/reference/provider-api"}}}')}}]);
