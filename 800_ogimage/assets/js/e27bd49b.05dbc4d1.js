3f2b71bbe724775caadc64013752666113bf8edd
joaniefromtheblock <joaniefromtheblock@users.noreply.github.com>
2024-01-24 19:44:34 +0000

    deploy: d808b3c801daa599bfe678913021421d394d3391

---
+++ 800_ogimage/assets/js/e27bd49b.05dbc4d1.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmetamask_docs=self.webpackChunkmetamask_docs||[]).push([[6128],{89263:e=>{e.exports=JSON.parse('{"title":"JSON-RPC API","slug":"wallet/reference/json-rpc-api","permalink":"/800_ogimage/wallet/reference/json-rpc-api","navigation":{"previous":{"title":"Ethereum provider API","permalink":"/800_ogimage/wallet/reference/provider-api"}}}')}}]);
