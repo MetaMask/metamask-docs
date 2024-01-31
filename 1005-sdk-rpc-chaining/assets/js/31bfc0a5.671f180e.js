136d91422acbdc4ce16ed23dfaf6fdf555873c31
alexandratran <alexandratran@users.noreply.github.com>
2024-01-31 18:53:39 +0000

    deploy: 195e8c7dee4f0199da5166e9230a08871eceeb0f

--- 1005-sdk-rpc-chaining/assets/js/31bfc0a5.671f180e.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmetamask_docs=self.webpackChunkmetamask_docs||[]).push([[7698],{64312:s=>{s.exports=JSON.parse('[{"label":"Keyring API","permalink":"/1005-sdk-rpc-chaining/snaps/tags/keyring-api","count":6}]')}}]);
