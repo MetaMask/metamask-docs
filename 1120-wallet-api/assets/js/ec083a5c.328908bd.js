b7a1ab35b2f8fa5f11975527dbf0de5386837d15
alexandratran <alexandratran@users.noreply.github.com>
2024-02-06 19:48:43 +0000

    deploy: 3bc92a4b5f28e9c7247628f0e6246084b9a1e43a

---
+++ 1120-wallet-api/assets/js/ec083a5c.328908bd.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmetamask_docs=self.webpackChunkmetamask_docs||[]).push([[4375],{27332:e=>{e.exports=JSON.parse('{"title":"JSON-RPC API","slug":"wallet/reference/json-rpc-api","permalink":"/1120-wallet-api/wallet/reference/json-rpc-api","navigation":{"previous":{"title":"Ethereum provider API","permalink":"/1120-wallet-api/wallet/reference/provider-api"}}}')}}]);
