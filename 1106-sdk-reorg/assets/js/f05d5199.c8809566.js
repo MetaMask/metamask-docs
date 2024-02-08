50acae9141b8803fc282016d36dc37df63fb7e8f
alexandratran <alexandratran@users.noreply.github.com>
2024-02-08 00:25:48 +0000

    deploy: a95851e9ffaeec86fbb067b64954976e7710895b

---
+++ 1106-sdk-reorg/assets/js/f05d5199.c8809566.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmetamask_docs=self.webpackChunkmetamask_docs||[]).push([[1938],{52587:e=>{e.exports=JSON.parse('{"title":"JSON-RPC API","slug":"wallet/reference/json-rpc-api","permalink":"/1106-sdk-reorg/wallet/reference/json-rpc-api","navigation":{"previous":{"title":"Ethereum provider API","permalink":"/1106-sdk-reorg/wallet/reference/provider-api"}}}')}}]);
