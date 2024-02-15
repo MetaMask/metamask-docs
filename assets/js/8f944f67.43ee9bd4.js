039d25b7aad83071c7ff42ea18da76e625ba815d
alexandratran <alexandratran@users.noreply.github.com>
2024-02-15 21:22:51 +0000

    deploy: 6b3b924bc9c86e6be87d3a31f369ea215ccd9b10

---
+++ assets/js/8f944f67.43ee9bd4.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmetamask_docs=self.webpackChunkmetamask_docs||[]).push([[5015],{20285:e=>{e.exports=JSON.parse('{"title":"Reference","slug":"/reference","permalink":"/snaps/reference","navigation":{"previous":{"title":"Connect to a Snap","permalink":"/snaps/how-to/connect-to-a-snap"},"next":{"title":"Snaps API","permalink":"/snaps/reference/snaps-api"}}}')}}]);
