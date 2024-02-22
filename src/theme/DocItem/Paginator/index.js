754084cf85347bfd20a2161a74fb14eb339d848f
joan e <joan.edwards@consensys.net>
2024-02-22 10:55:02 -0800

    updates to docs components

---
+++ src/theme/DocItem/Paginator/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export default function PaginatorWrapper(props) {
+  return null;
+  
+}
---
+++ src/theme/TagsListInline/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TagsListInline from '@theme-original/TagsListInline';
+
+export default function TagsListInlineWrapper(props) {
+  const wrapperStyle = { fontSize: '0.75rem' };
+
+  return (
+    <div style={wrapperStyle}>
+      <TagsListInline {...props} />
+    </div>
+  );
+}
