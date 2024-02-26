96f7685f2784575d702273e0adc07989a292511c
Joan E <153745173+joaniefromtheblock@users.noreply.github.com>
2024-02-26 12:44:36 -0800

    slightly larger

--- src/theme/EditThisPage/index.js
+++ src/theme/EditThisPage/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import EditThisPage from '@theme-original/EditThisPage';
 
 export default function EditThisPageWrapper(props) {
-  const wrapperStyle = { fontSize: '0.75rem' };
+  const wrapperStyle = { fontSize: '0.85rem' };
 
   return (
     <div style={wrapperStyle}>
