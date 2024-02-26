42cb4554a892ad32c30152c10f8e5e6578c7a122
Joan E <153745173+joaniefromtheblock@users.noreply.github.com>
2024-02-26 12:44:20 -0800

    make slightly larger

--- src/theme/TagsListInline/index.js
+++ src/theme/TagsListInline/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import TagsListInline from '@theme-original/TagsListInline';
 
 export default function TagsListInlineWrapper(props) {
-  const wrapperStyle = { fontSize: '0.75rem' };
+  const wrapperStyle = { fontSize: '0.85rem' };
 
   return (
     <div style={wrapperStyle}>
