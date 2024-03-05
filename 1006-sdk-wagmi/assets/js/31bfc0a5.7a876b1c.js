2a36ff42864d423aabeb3330b246c3b0529b7f28
alexandratran <alexandratran@users.noreply.github.com>
2024-03-05 04:04:08 +0000

    deploy: efc9a093eb55201884a1fb517a9bdffd894e781f

--- 1006-sdk-wagmi/assets/js/31bfc0a5.7a876b1c.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmetamask_docs=self.webpackChunkmetamask_docs||[]).push([[7698],{64312:s=>{s.exports=JSON.parse('[{"label":"Keyring API","permalink":"/1006-sdk-wagmi/snaps/tags/keyring-api","count":5}]')}}]);
--- 1006-sdk-wagmi/assets/js/84a7ba25.730a0235.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmetamask_docs=self.webpackChunkmetamask_docs||[]).push([[1763],{47743:e=>{e.exports=JSON.parse('{"title":"JSON-RPC API","slug":"wallet/reference/json-rpc-api","permalink":"/1006-sdk-wagmi/wallet/reference/json-rpc-api","navigation":{"previous":{"title":"Ethereum provider API","permalink":"/1006-sdk-wagmi/wallet/reference/provider-api"}}}')}}]);
