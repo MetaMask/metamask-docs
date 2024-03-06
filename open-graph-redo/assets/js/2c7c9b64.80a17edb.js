47275626684d1a63c5a1ca391d53286ad0615b23
joaniefromtheblock <joaniefromtheblock@users.noreply.github.com>
2024-03-06 19:55:53 +0000

    deploy: 1dbf5f2cdb14b35651e81c76cad2a08075b6bf53

--- open-graph-redo/assets/js/2c7c9b64.80a17edb.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmetamask_docs=self.webpackChunkmetamask_docs||[]).push([[7567],{20567:e=>{e.exports=JSON.parse('{"title":"JSON-RPC API","slug":"wallet/reference/json-rpc-api","permalink":"/open-graph-redo/wallet/reference/json-rpc-api","navigation":{"previous":{"title":"Ethereum provider API","permalink":"/open-graph-redo/wallet/reference/provider-api"}}}')}}]);
