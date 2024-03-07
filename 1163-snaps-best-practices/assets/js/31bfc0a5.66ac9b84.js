474d5b94c27e58e27c208c73408988c8547003a4
alexandratran <alexandratran@users.noreply.github.com>
2024-03-07 00:14:13 +0000

    deploy: 7a4fdf5caa764027d1b40b20210323fcfc77216e

---
+++ 1163-snaps-best-practices/assets/js/31bfc0a5.66ac9b84.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmetamask_docs=self.webpackChunkmetamask_docs||[]).push([[7698],{64312:s=>{s.exports=JSON.parse('[{"label":"Keyring API","permalink":"/1163-snaps-best-practices/snaps/tags/keyring-api","count":9}]')}}]);
