33eef111e69947584acb9897c0de6089f7e837c6
Alexandra Tran <alexandratran@protonmail.com>
2024-03-07 15:39:08 -0800

    move markdown styles to separate sheet

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -67,7 +67,7 @@ const config = {
           },
         },
         theme: {
-          customCss: require.resolve("./src/css/custom.css"),
+          customCss: [require.resolve("./src/css/custom.css"), require.resolve("./src/css/styles.css")],
         },
       }),
     ],
