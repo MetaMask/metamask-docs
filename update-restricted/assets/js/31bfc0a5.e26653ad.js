950b2d3e5984cfea6f709b4b1fcee3d52a2cbb2d
joaniefromtheblock <joaniefromtheblock@users.noreply.github.com>
2024-03-20 20:41:52 +0000

    deploy: 07f1eb5a9a81cecf2ce995dbe3b01a5db287cdd3

---
+++ update-restricted/assets/js/31bfc0a5.e26653ad.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmetamask_docs=self.webpackChunkmetamask_docs||[]).push([[7698],{64312:e=>{e.exports=JSON.parse('[{"label":"Keyring API","permalink":"/update-restricted/snaps/tags/keyring-api","count":9}]')}}]);
