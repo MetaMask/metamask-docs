2dd2067f4c1e1c3036b3acc1e674a0eb30ffef1d
alexandratran <alexandratran@users.noreply.github.com>
2024-03-26 19:09:37 +0000

    deploy: 882fc321596c3653ce5c1bab0493a4322a779366

--- 1220-update-whats-new/assets/js/d3bb2b22.cee5026f.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmetamask_docs=self.webpackChunkmetamask_docs||[]).push([[3140],{37371:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"docs"}')}}]);
