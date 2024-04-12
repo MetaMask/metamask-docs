ef8c7306868460e86252b0d1acdc138a97177280
Montoya <Montoya@users.noreply.github.com>
2024-04-12 14:04:41 +0000

    deploy: bb00bdcc7f141de8f193819a138399ea52eadfbb

---
+++ update-bep-resource/assets/js/31bfc0a5.2cdc2b7e.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmetamask_docs=self.webpackChunkmetamask_docs||[]).push([[7698],{64312:e=>{e.exports=JSON.parse('[{"label":"Keyring API","permalink":"/update-bep-resource/snaps/tags/keyring-api","count":9}]')}}]);
