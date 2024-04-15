eeb521c52d2c5329725499e41ac8379038b76310
alexandratran <alexandratran@users.noreply.github.com>
2024-04-15 19:23:59 +0000

    deploy: 83ac24c46a3e5a714359e0a2777ade4836cc4709

--- fix-spinner-gif/assets/js/d7f46032.21d31a38.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmetamask_docs=self.webpackChunkmetamask_docs||[]).push([[577],{70991:e=>{e.exports=JSON.parse('{"title":"JSON-RPC API","slug":"wallet/reference/json-rpc-api","permalink":"/fix-spinner-gif/wallet/reference/json-rpc-api","navigation":{"previous":{"title":"Ethereum provider API","permalink":"/fix-spinner-gif/wallet/reference/provider-api"}}}')}}]);
