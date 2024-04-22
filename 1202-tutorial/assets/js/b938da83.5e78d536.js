ef506b75008fb3a80c07e083192cfccbd8275d89
joaniefromtheblock <joaniefromtheblock@users.noreply.github.com>
2024-04-22 18:00:28 +0000

    deploy: 62e561c76fa716b5be87e12bb42dc30ef4884367

---
+++ 1202-tutorial/assets/js/b938da83.5e78d536.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmetamask_docs=self.webpackChunkmetamask_docs||[]).push([[5643],{85331:e=>{e.exports=JSON.parse('{"title":"JSON-RPC API","slug":"wallet/reference/json-rpc-api","permalink":"/1202-tutorial/wallet/reference/json-rpc-api","navigation":{"previous":{"title":"Ethereum provider API","permalink":"/1202-tutorial/wallet/reference/provider-api"}}}')}}]);
