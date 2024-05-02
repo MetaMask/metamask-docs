3ec530e809f568405fd9f12c87353a5aea2654c1
alexandratran <alexandratran@users.noreply.github.com>
2024-05-02 18:39:39 +0000

    deploy: 67fbd198338b1e9c81ba6533378bc766bcb888c5

--- update-i18n/assets/js/f08a938e.08cba359.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmetamask_docs=self.webpackChunkmetamask_docs||[]).push([[7253],{90347:e=>{e.exports=JSON.parse('{"title":"JSON-RPC API","slug":"wallet/reference/json-rpc-api","permalink":"/update-i18n/wallet/reference/json-rpc-api","navigation":{"previous":{"title":"Ethereum provider API","permalink":"/update-i18n/wallet/reference/provider-api"}}}')}}]);
