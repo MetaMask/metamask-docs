91b98a176b63e1803a4fa5d60edd9871e610af69
alexandratran <alexandratran@users.noreply.github.com>
2024-05-02 18:30:51 +0000

    deploy: e52b1bba65ff6464da3bf0f07e0ce479997bca19

---
+++ update-i18n/assets/js/31bfc0a5.39543e2c.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmetamask_docs=self.webpackChunkmetamask_docs||[]).push([[6832],{14806:e=>{e.exports=JSON.parse('[{"label":"Keyring API","permalink":"/update-i18n/snaps/tags/keyring-api","count":9}]')}}]);
