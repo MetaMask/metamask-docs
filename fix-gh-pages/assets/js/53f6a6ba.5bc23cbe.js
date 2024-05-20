c4b7d0e0ada2342ec3d101450b099f92b4a052c9
Montoya <Montoya@users.noreply.github.com>
2024-05-20 19:23:13 +0000

    deploy: 5793c4f30e4c1399612bb3351af55023bb1a084d

---
+++ fix-gh-pages/assets/js/53f6a6ba.5bc23cbe.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmetamask_docs=self.webpackChunkmetamask_docs||[]).push([[2809],{61531:e=>{e.exports=JSON.parse('{"title":"JSON-RPC API","slug":"wallet/reference/json-rpc-api","permalink":"/fix-gh-pages/wallet/reference/json-rpc-api","navigation":{"previous":{"title":"Ethereum provider API","permalink":"/fix-gh-pages/wallet/reference/provider-api"}}}')}}]);
