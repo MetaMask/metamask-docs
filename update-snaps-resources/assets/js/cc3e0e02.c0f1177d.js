19319223dcb02a939bc23768f9ca791ef2ef4453
Montoya <Montoya@users.noreply.github.com>
2024-06-04 19:53:41 +0000

    deploy: b10930f2a3f4965617d6770cf012745bc7a276cf

--- update-snaps-resources/assets/js/cc3e0e02.c0f1177d.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmetamask_docs=self.webpackChunkmetamask_docs||[]).push([[7674],{58586:s=>{s.exports=JSON.parse('{"tags":[{"label":"Keyring API","permalink":"/update-snaps-resources/snaps/tags/keyring-api","count":9}]}')}}]);
