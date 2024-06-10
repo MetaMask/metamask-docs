4d35de31dd56102e11737265ed2e9031fc3f5f13
Magne Cedric <cedric.magne@consensys.net>
2024-06-10 11:52:39 +0200

    feat(launchdarkly): fix build when window is undefined

--- src/plugins/launchdarkly/ldClient.ts
+++ src/plugins/launchdarkly/ldClient.ts
@@ -1,17 +1,12 @@
 import * as LDClient from "launchdarkly-js-client-sdk";
 
-let ldClient;
-
-if (typeof window !== "undefined") {
-  ldClient = LDClient.initialize(process.env.LD_CLIENT_ID, {
-    key: "anonymous-user",
-  });
+const ldClient = LDClient.initialize(process.env.LD_CLIENT_ID, {
+  key: "anonymous-user",
+});
   
-  ldClient.on("ready", () => {
-    console.log("LaunchDarkly client ready");
-  });  
-} else {
-  console.log("LaunchDarkly client not loaded - window is not defined");
-}
+ldClient.on("ready", () => {
+  console.log("LaunchDarkly client ready");
+});  
+
 
 export default ldClient;
