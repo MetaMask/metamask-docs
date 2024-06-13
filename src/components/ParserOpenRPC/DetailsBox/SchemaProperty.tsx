6c553f8e3d0b7b50bff539832331a0ed7bdf7739
TrofimovAnton85 <98453427+TrofimovAnton85@users.noreply.github.com>
2024-06-13 10:20:22 +0200

    feat(docs): added rpc-parser component for reference pages (#1331)
    * feat(docs): added rpc-parser component for reference pages
    
    * feat(docs): added mm install msg
    
    * feat(docs): change mm condition
    
    * feat(docs): refactored styles
    
    * feat(docs): added request/response section
    
    * feat(docs): added error section
    
    * feat(docs): added eth_call
    
    * feat(docs): added tag labels
    
    * feat(docs): added result section
    
    * feat(docs): added condition for prod
    
    * feat(docs): revert condition for prod
    
    * feat(docs): fixes after review
    
    * feat(docs): renamed to tsx
    
    * feat(docs): fixes for styles & added props
    
    * feat(docs): hide page from sidebar

---
+++ src/components/ParserOpenRPC/DetailsBox/SchemaProperty.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { MDContent } from "./MDContent";
+import styles from "./styles.module.css";
+
+interface SchemaPropertyProps {
+  title: string;
+  type?: string;
+  required?: boolean;
+  description?: string;
+}
+
+interface TagProps {
+  name: string;
+}
+
+export const SchemaProperty = ({ title, type, required, description }: SchemaPropertyProps) => {
+  return (
+    <div className="padding-vert--md">
+      <div
+        className="row row--align-center row--no-gutters padding-bottom--sm"
+        style={{ justifyContent: "space-between" }}
+      >
+        <div>
+          <strong>{title}</strong>
+          <span className={styles.textAltColor}> {type} </span>
+        </div>
+        {required && <span className={styles.textErrorColor}>required</span>}
+      </div>
+      <p className="margin--none">
+        <MDContent content={description} />
+      </p>
+    </div>
+  );
+};
+
+export const Tag = ({ name }: TagProps) => {
+  const bgStyle = {
+    "MetaMask": "#4DB6AC",
+    "Restricted": "#FFECB3",
+    "Deprecated": "#7E57C2",
+  };
+  return (
+    <div
+      className={styles.tagItem}
+      style={{ backgroundColor: `${bgStyle[name] ? bgStyle[name] : "#FFCDD2"}` }}
+    >
+      {name}
+    </div>
+  );
+};
