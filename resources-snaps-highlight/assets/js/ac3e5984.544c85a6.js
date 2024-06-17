8c83312a409fb66ccc3a4703ac5f6c83b80dc219
joaniefromtheblock <joaniefromtheblock@users.noreply.github.com>
2024-06-17 15:44:52 +0000

    deploy: 4fc2a78c6d4fb87629f3c664a8f3a27f46e6040a

---
+++ resources-snaps-highlight/assets/js/ac3e5984.544c85a6.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmetamask_docs=self.webpackChunkmetamask_docs||[]).push([[5133],{79336:s=>{s.exports=JSON.parse('{"tags":[{"label":"Keyring API","permalink":"/resources-snaps-highlight/snaps/tags/keyring-api","count":9}]}')}}]);
