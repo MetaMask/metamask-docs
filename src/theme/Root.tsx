c65cb98e5897f3924f60737ddd8b34b5854168d5
ae_atrofimov <anton.trofimov-ext@consensys.net>
2024-07-03 17:54:16 +0200

    feat(wip): fix build

--- src/theme/Root.tsx
+++ src/theme/Root.tsx
@@ -1,20 +1,30 @@
 import React, { ReactChild } from "react";
 import { MetaMaskProvider } from '@metamask/sdk-react';
+import BrowserOnly from '@docusaurus/BrowserOnly';
 
 export default function Root({ children }: { children: ReactChild}) {
   return (
-    <MetaMaskProvider debug={false} sdkOptions={{
-      logging:{
-          developerMode: false,
-        },
-        checkInstallationImmediately: false,
-        dappMetadata: {
-          name: "Test app",
-          url: window.location.host,
-        },
-        preferDesktop: true
-    }}>
-      {children}
-    </MetaMaskProvider>
+    <BrowserOnly fallback={<div>Loading...</div>}>
+      {
+        () => {
+          return (
+            <MetaMaskProvider debug={false} sdkOptions={{
+              logging:{
+                  developerMode: false,
+                },
+                checkInstallationImmediately: false,
+                dappMetadata: {
+                  name: "Test app",
+                  url: window.location.host,
+                },
+                preferDesktop: true
+            }}>
+              {children}
+            </MetaMaskProvider>
+          )
+        }
+      }
+    </BrowserOnly>
+    
   );
 }
