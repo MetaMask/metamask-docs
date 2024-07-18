c88cbc372a90703f97e6eda725ccb3ed69ac9363
ae_atrofimov <anton.trofimov-ext@consensys.net>
2024-07-18 13:52:35 +0200

    feat(docs): added js-cookie

--- src/plugins/launchdarkly/ldClient.ts
+++ src/plugins/launchdarkly/ldClient.ts
@@ -1,6 +1,7 @@
 import { initialize } from "launchdarkly-js-client-sdk";
 import ExecutionEnvironment from "@docusaurus/ExecutionEnvironment";
 import siteConfig from "@generated/docusaurus.config";
+import Cookies from "js-cookie";
 
 export default (function () {
   if (!ExecutionEnvironment.canUseDOM) {
@@ -8,19 +9,14 @@ export default (function () {
   }
 
   const { LD_CLIENT_ID } = siteConfig.customFields;
-
-  const getCookie = (name: string) => {
-    let matches = document.cookie.match(new RegExp(
-      "(?:^|; )" + name.replace(/([.$?*|{}()[]\/+^])/g, '\\$1') + "=([^;]*)"
-    ));
-    return matches ? decodeURIComponent(matches[1]) : undefined;
-  }
+  const COOKIE_NAME = "feature_preview";
+  const feature_preview = Cookies.get(COOKIE_NAME);
 
   const context = {
     kind: "user",
     anonymous: true,
     key: "ld-anonymous-user-key",
-    feature_preview: getCookie("feature_preview"),
+    ...(feature_preview && { feature_preview }),
   };
 
   const ldClient = initialize(LD_CLIENT_ID as string, context, {
