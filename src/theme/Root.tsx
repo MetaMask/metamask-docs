c0aac4452f5c423b82f169c0162f94019c0fc745
Magne Cedric <cedric.magne@consensys.net>
2024-07-31 18:02:24 +0200

    feat(m-login): fix build

--- src/theme/Root.tsx
+++ src/theme/Root.tsx
@@ -1,24 +1,29 @@
 import React, { ReactChild } from "react";
 import { MetaMaskProvider } from "@metamask/sdk-react";
+import BrowserOnly from "@docusaurus/BrowserOnly";
 
 export default function Root({ children }: { children: ReactChild }) {
   return (
-    <MetaMaskProvider
-      debug={false}
-      sdkOptions={{
-        checkInstallationOnAllCalls: true,
-        extensionOnly: true,
-        preferDesktop: true,
-        logging: {
-          sdk: false,
-        },
-        dappMetadata: {
-          name: "Reference pages",
-          url: window.location.href,
-        },
-      }}
-    >
-      {children}
-    </MetaMaskProvider>
+    <BrowserOnly>
+      {() => (
+        <MetaMaskProvider
+          debug={false}
+          sdkOptions={{
+            checkInstallationOnAllCalls: true,
+            extensionOnly: true,
+            preferDesktop: true,
+            logging: {
+              sdk: false,
+            },
+            dappMetadata: {
+              name: "Reference pages",
+              url: window.location.href,
+            },
+          }}
+        >
+          {children}
+        </MetaMaskProvider>
+      )}
+    </BrowserOnly>
   );
 }
