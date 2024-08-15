a8b0d6d3b005e074d3cdfca542199b82e055878b
Aslan A <ak.fordev@gmail.com>
2024-08-15 22:02:34 +0500

    refactor(CallToAction): update Button component

--- src/components/elements/call-to-action/index.js
+++ src/components/elements/call-to-action/index.js
@@ -1,5 +1,5 @@
 import { clsx } from 'clsx'
-import Button from '@site/src/components/elements/button'
+import Button from '@site/src/components/elements/buttons/button'
 import CutOffCorners from '@site/src/components/elements/cut-off-corners'
 import Heading from '@theme/Heading'
 
@@ -7,7 +7,7 @@ import Shape from '@site/static/img/shapes/hero/shape.svg'
 
 import styles from './call-to-action.module.scss'
 
-const CallToAction = ({ title, description, link }) => {
+const CallToAction = ({ title, description, button }) => {
   return (
     <section className={clsx(styles['call-to-action'])}>
       <div className={clsx(styles['container'], 'container')}>
@@ -24,8 +24,8 @@ const CallToAction = ({ title, description, link }) => {
                     <p className={clsx(styles['description'], 'type-paragraph-m')}>{description}</p>
                   )}
 
-                  {link && (
-                    <Button className={styles['button']} to={link.to} label={link.label} isLight />
+                  {button && (
+                    <Button className={styles['button']} as="link" type="secondary" {...button} />
                   )}
                 </div>
                 <div className={styles['grid-col-right']}>
