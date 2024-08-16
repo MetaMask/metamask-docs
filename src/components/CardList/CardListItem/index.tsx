7341474d482e1f29f6abd67b7a1b36621aa3c379
Aslan A <ak.fordev@gmail.com>
2024-08-16 22:33:30 +0500

    refactor(CardList): update component styles and structure

--- src/components/CardList/CardListItem/index.tsx
+++ src/components/CardList/CardListItem/index.tsx
@@ -1,44 +1,76 @@
-import React, { ReactNode } from "react";
-import clsx from "clsx";
-import Link from "@docusaurus/Link";
-import styles from "./styles.module.css";
+import React, { ReactNode } from 'react'
+import clsx from 'clsx'
+import Link from '@docusaurus/Link'
+
+import { useState, useEffect } from 'react'
+import { useColorMode } from '@docusaurus/theme-common'
+import Heading from '@theme/Heading'
+import CutOffCorners from '@site/src/components/elements/cut-off-corners'
+import Button from '@site/src/components/elements/buttons/button'
+
+import styles from './styles.module.scss'
 
 export interface CardItem {
-  title?: ReactNode;
-  description?: ReactNode;
-  href?: string;
-  icon?: string;
-  flaskOnly?: boolean;
+  title?: ReactNode
+  description?: ReactNode
+  href?: string
+  icon?: string
+  flaskOnly?: boolean
 }
 
 interface CardListItemProps {
-  item: CardItem;
+  item: CardItem
 }
 
-export default function CardListItem({ item }: CardListItemProps): JSX.Element {
-  const cardContent = (
-    <>
-      {item.title && (
-        <h2 className={clsx(styles.cardTitle)}>
-          {item.icon && <img src={item.icon} className={styles.cardIcon} />}
-          <span>{item.title}</span>
-        </h2>
-      )}
-      {item.description && <p>{item.description}</p>}
-    </>
-  );
+export default function CardListItem({ item }: CardListItemProps) {
+  const { title, description, href, icon } = item
+  const [isHovered, setIsHovered] = useState(false)
+  const { colorMode } = useColorMode()
+  const [theme, setTheme] = useState('')
 
-  const rootClassName = clsx("card", "padding--lg", styles.cardContainer, {
-    [styles.flaskOnly]: item.flaskOnly,
-  });
+  useEffect(() => {
+    setTheme(colorMode)
+  }, [colorMode])
 
-  if (item.href) {
-    return (
-      <Link className={rootClassName} href={item.href}>
-        {cardContent}
-      </Link>
-    );
-  }
+  return (
+    <li className={clsx(styles['item'], isHovered && styles['active'])}>
+      <CutOffCorners size="s">
+        <div
+          className={styles['holder']}
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}>
+          <Link to={href} className={clsx(styles['inner'], 'text-decoration-none')}>
+            <div className={styles['header']}>
+              <Heading as="h3" className={clsx(styles['title'], 'type-heading-xs')}>
+                {icon && <img src={icon} className={styles.icon} />}
+                <span>{title}</span>
+              </Heading>
 
-  return <div className={rootClassName}>{cardContent}</div>;
+              <p className={clsx(styles['description'], 'type-paragraph-s')}>{description}</p>
+            </div>
+            {href && (
+              <Button
+                as="div"
+                label={false}
+                type={theme === 'dark' ? 'secondary' : 'primary'}
+                icon="arrow-right"
+                className={styles['button']}
+                style={
+                  theme === 'dark'
+                    ? {
+                        '--button-color-hover': 'var(--general-black)',
+                        '--button-text-color-hover': 'var(--general-white)',
+                      }
+                    : {
+                        '--button-color-hover': 'var(--general-black)',
+                        '--button-text-color-hover': 'var(--general-white)',
+                      }
+                }
+              />
+            )}
+          </Link>
+        </div>
+      </CutOffCorners>
+    </li>
+  )
 }
--- src/components/CardList/index.tsx
+++ src/components/CardList/index.tsx
@@ -1,20 +1,21 @@
-import React from "react";
-import CardListItem, { CardItem } from "./CardListItem";
+import CardListItem, { CardItem } from './CardListItem'
+
+import styles from './CardList.module.scss'
 
 interface CardListProps {
-  items: CardItem[];
+  items: CardItem[]
 }
 
 export default function CardList(props: CardListProps): JSX.Element {
-  const { items } = props;
+  const { items } = props
 
   return (
-    <section className="row">
-      {items.map((item, index) => (
-        <article key={index} className="col col--6 margin-bottom--lg">
-          <CardListItem item={item} />
-        </article>
-      ))}
+    <section className={styles['wrapper']}>
+      <ul className={styles['list']}>
+        {items.map((item, index) => (
+          <CardListItem key={index} item={item} />
+        ))}
+      </ul>
     </section>
-  );
+  )
 }
