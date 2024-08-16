187490e748fd79dc5bad85e64013e2f30b0fd2d2
Aslan A <ak.fordev@gmail.com>
2024-08-16 23:13:09 +0500

    refactor(Card): update button hover styles

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -61,8 +61,8 @@ export default function Card({ title, href, description }: CardItem) {
                   style={
                     theme === 'dark'
                       ? {
-                          '--button-color-hover': 'var(--general-black)',
-                          '--button-text-color-hover': 'var(--general-white)',
+                          '--button-color-hover': 'var(--general-white)',
+                          '--button-text-color-hover': 'var(--general-black)',
                         }
                       : {
                           '--button-color-hover': 'var(--general-black)',
