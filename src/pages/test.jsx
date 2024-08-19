f12ac7b659a031d811f9ed4b45deba1bf80a903e
Aslan A <ak.fordev@gmail.com>
2024-08-19 19:56:43 +0500

    refactor(test page): Add SnapsSection component

--- src/pages/test.jsx
+++ src/pages/test.jsx
@@ -1,6 +1,7 @@
 import Layout from '@theme/Layout'
 import Heading from '@theme/Heading'
 import Pill from '@site/src/components/Pill'
+import SnapsSection from '@site/src/components/SnapsSection'
 
 export default function Test() {
   return (
@@ -40,6 +41,37 @@ export default function Test() {
           multiple wallets in the user's browser. We recommend detecting multiple wallets to improve
           the user experience.
         </Pill>
+
+        <SnapsSection
+          items={[
+            {
+              colorPalette: 'purple',
+              title: 'Extend the functionality of MetaMask using Snaps',
+              description:
+                'A Snap is a JavaScript program run in an isolated environment that customizes the MetaMask wallet experience. You can create a Snap that adds new API methods, adds support for different blockchain protocols, or modifies existing functionalities.',
+              cards: [
+                {
+                  title: 'Snaps quickstart',
+                  description:
+                    'Get started quickly by creating and customizing a simple Snap, using the Snaps template built with React and TypeScript.',
+                  href: '/snaps/get-started/quickstart',
+                },
+                {
+                  title: 'Snaps tutorials',
+                  description:
+                    'Follow the step-by-step tutorials to create Snaps that estimate gas fees, provide transaction insights with custom UI, and more.',
+                  href: '/snaps/learn/tutorials',
+                },
+                {
+                  title: 'Snaps API',
+                  description:
+                    'Use the Snaps API to modify the functionality of MetaMask and communicate between dapps and Snaps.',
+                  href: '/snaps/learn/tutorials',
+                },
+              ],
+            },
+          ]}
+        />
       </div>
     </Layout>
   )
