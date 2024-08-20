fcc077bb563ece8742885ff1ce1fd1efd75563ee
Aslan A <ak.fordev@gmail.com>
2024-08-20 15:49:27 +0500

    refactor(CodeTerminal): update styles and structure for CodeTerminal components

--- src/components/CodeTerminal/ControlPanel.jsx
+++ src/components/CodeTerminal/ControlPanel.jsx
@@ -1,117 +1,105 @@
-import React, { useMemo, useState, useEffect } from "react";
-import Select from "react-dropdown-select";
-import Icon from "@site/src/components/Icon/Icon";
-import {
-  NETWORKS_NAMES,
-  NETWORKS_METHODS,
-  NETWORKS,
-} from "@site/src/lib/constants";
+import clsx from 'clsx'
+import { useMemo, useState, useEffect } from 'react'
+import Select from 'react-dropdown-select'
+import Icon from '@site/src/components/Icon/Icon'
+import { NETWORKS_NAMES, NETWORKS_METHODS, NETWORKS } from '@site/src/lib/constants'
+
+import styles from './ControlPanel.module.scss'
 
 const ControlPanel = ({ keysOptions = [], initValues, onChange }) => {
   const selectChangeHandler = (value, param) => {
-    onChange(value, param);
-  };
+    onChange(value, param)
+  }
 
   const typesOptions = useMemo(() => {
-    const initMethods = [...NETWORKS_METHODS.L1.methods];
-    const initL2Methods = [...NETWORKS_METHODS.L2.methods];
+    const initMethods = [...NETWORKS_METHODS.L1.methods]
+    const initL2Methods = [...NETWORKS_METHODS.L2.methods]
     let updatedOptions = {
       types: [],
       methods: initMethods,
-    };
+    }
     if (initValues?.netName?.networksTypes) {
-      updatedOptions.types = initValues.netName.networksTypes;
+      updatedOptions.types = initValues.netName.networksTypes
     }
     if (initValues?.netName?.value === NETWORKS.PALM) {
-      const noEthMethods = initMethods.filter(
-        (method) => method.value !== "eth_maxPriorityFeePerGas"
-      );
-      updatedOptions.methods = [...noEthMethods];
+      const noEthMethods = initMethods.filter(method => method.value !== 'eth_maxPriorityFeePerGas')
+      updatedOptions.methods = [...noEthMethods]
     }
     if (initValues?.netName?.value === NETWORKS.GAS_API) {
-      updatedOptions.methods = [...initL2Methods];
+      updatedOptions.methods = [...initL2Methods]
     }
-    return updatedOptions;
-  }, [initValues.netName]);
+    return updatedOptions
+  }, [initValues.netName])
 
-  const [methodValue, setMethodValue] = useState([typesOptions.methods[0]]);
+  const [methodValue, setMethodValue] = useState([typesOptions.methods[0]])
   useEffect(() => {
-    const upMethod =
-      methodValue.length > 0 ? methodValue[0] : typesOptions.methods[0];
-    selectChangeHandler(upMethod, "netMethod");
-  }, [methodValue, typesOptions]);
+    const upMethod = methodValue.length > 0 ? methodValue[0] : typesOptions.methods[0]
+    selectChangeHandler(upMethod, 'netMethod')
+  }, [methodValue, typesOptions])
 
   return (
-    <div className="flex-row">
-      <div className="flex-col w-lg-25">
-        <div className="select-wrap">
-          <Icon name="key" classes="select-icon" />
-          <Select
-            className="custom-select"
-            placeholder="Key name"
-            values={[initValues.apiKey]}
-            searchable={false}
-            options={keysOptions}
-            onChange={(values) => selectChangeHandler(values[0], "apiKey")}
-          />
-        </div>
-      </div>
-      <div className="flex-col w-lg-25">
-        <div className="select-wrap">
-          <Icon name="network" classes="select-icon" />
-          <Select
-            className="custom-select"
-            placeholder="Select network"
-            values={[initValues.netName]}
-            searchable={false}
-            options={NETWORKS_NAMES}
-            onChange={(values) => {
-              const netName = values[0];
-              selectChangeHandler(netName, "netName");
-              selectChangeHandler(netName.networksTypes[0], "netType");
-              if (netName.value === NETWORKS.GAS_API) {
-                setMethodValue([NETWORKS_METHODS.L2.methods[0]]);
-              } else {
-                setMethodValue([NETWORKS_METHODS.L1.methods[0]]);
-              }
-            }}
-          />
-        </div>
-      </div>
-      <div className="flex-col w-lg-25">
-        <div className="select-wrap">
-          <Icon name="type" classes="select-icon" />
-          <Select
-            className="custom-select"
-            placeholder="Select type"
-            values={[initValues.netType]}
-            searchable={false}
-            options={typesOptions.types}
-            onChange={(values) => selectChangeHandler(values[0], "netType")}
-          />
-        </div>
-      </div>
-      <div className="flex-col w-lg-25">
-        <div className="select-wrap">
-          <Icon name="method" classes="select-icon" />
-          <Select
-            className="custom-select"
-            placeholder="Select method"
-            values={methodValue}
-            options={typesOptions.methods}
-            onChange={(values) => {
-              setMethodValue(values);
-            }}
-            onDropdownClose={() => {
-              if (methodValue.length === 0) {
-                setMethodValue([typesOptions.methods[0]]);
-              }
-            }}
-          />
-        </div>
-      </div>
-    </div>
-  );
-};
+    <ul className={clsx(styles['holder'], 'list-styles-none')}>
+      <li className={styles['item']}>
+        <Icon name="key" />
+        <Select
+          className={styles['custom-select']}
+          placeholder="Key name"
+          values={[initValues.apiKey]}
+          searchable={false}
+          options={keysOptions}
+          onChange={values => selectChangeHandler(values[0], 'apiKey')}
+        />
+      </li>
+      <li className={styles['item']}>
+        <Icon name="network" />
+        <Select
+          className={styles['custom-select']}
+          placeholder="Select network"
+          values={[initValues.netName]}
+          searchable={false}
+          options={NETWORKS_NAMES}
+          onChange={values => {
+            const netName = values[0]
+            selectChangeHandler(netName, 'netName')
+            selectChangeHandler(netName.networksTypes[0], 'netType')
+            if (netName.value === NETWORKS.GAS_API) {
+              setMethodValue([NETWORKS_METHODS.L2.methods[0]])
+            } else {
+              setMethodValue([NETWORKS_METHODS.L1.methods[0]])
+            }
+          }}
+        />
+      </li>
+      <li className={styles['item']}>
+        <Icon name="type" />
+        <Select
+          className={styles['custom-select']}
+          placeholder="Select type"
+          values={[initValues.netType]}
+          searchable={false}
+          options={typesOptions.types}
+          onChange={values => selectChangeHandler(values[0], 'netType')}
+        />
+      </li>
+      <li className={styles['item']}>
+        <Icon name="method" />
+        <Select
+          className={styles['custom-select']}
+          placeholder="Select method"
+          values={methodValue}
+          options={typesOptions.methods}
+          onChange={values => {
+            setMethodValue(values)
+          }}
+          onDropdownClose={() => {
+            if (methodValue.length === 0) {
+              setMethodValue([typesOptions.methods[0]])
+            }
+          }}
+        />
+      </li>
+    </ul>
+  )
+}
 
-export default ControlPanel;
+export default ControlPanel
--- src/components/CodeTerminal/TerminalViewBox.jsx
+++ src/components/CodeTerminal/TerminalViewBox.jsx
@@ -1,63 +1,109 @@
-import React, { useMemo } from "react";
-import Icon from "../Icon/Icon";
-import { API_URL } from "../../lib/constants";
-import Link from "@docusaurus/Link";
-import CodeBlock from "@theme/CodeBlock";
+import clsx from 'clsx'
+import { useMemo, useState, useEffect } from 'react'
+import { API_URL } from '../../lib/constants'
+import { useColorMode } from '@docusaurus/theme-common'
+import CodeBlock from '@theme/CodeBlock'
+import Button from '@site/src/components/elements/buttons/button'
+
+import styles from './TerminalViewBox.module.scss'
 
 const TerminalViewBox = ({
-  url = "{network}",
-  id = "{ID}",
-  method = "method",
+  url = '{network}',
+  id = '{ID}',
+  method = 'method',
   params = [],
   logged = false,
   hideFooter = false,
   response,
   isExpansionNetwork = false,
 }) => {
+  const { colorMode } = useColorMode()
+  const [theme, setTheme] = useState('')
+
+  useEffect(() => {
+    setTheme(colorMode)
+  }, [colorMode])
 
   const exampleRequest = useMemo(() => {
-    const prepareParams = params.length === 0 ? "" : params.map(param => {
-      if ('boolean' === typeof param) return `${param}`
-      return `"${param}"`
-    });
+    const prepareParams =
+      params.length === 0
+        ? ''
+        : params.map(param => {
+            if ('boolean' === typeof param) return `${param}`
+            return `"${param}"`
+          })
     if (isExpansionNetwork) {
-      return `curl -X 'GET' \\\n'https://${url}'`;
+      return `curl -X 'GET' \\\n'https://${url}'`
     }
-    return `curl https://${url}/v3/${id} \\\n  -X POST \\\n  -H "Content-Type: application/json" \\\n  -d '{\n    "jsonrpc": "2.0",\n    "method": "${method}",\n    "params": [${params.length === 0 ? "" : prepareParams}],\n    "id": 1\n  }'`
-  }, [url, id, method, params, isExpansionNetwork]);
+    return `curl https://${url}/v3/${id} \\\n  -X POST \\\n  -H "Content-Type: application/json" \\\n  -d '{\n    "jsonrpc": "2.0",\n    "method": "${method}",\n    "params": [${params.length === 0 ? '' : prepareParams}],\n    "id": 1\n  }'`
+  }, [url, id, method, params, isExpansionNetwork])
 
   return (
-    <div className="terminal-wrapper">
-      <CodeBlock language="cURL">
-        {exampleRequest}
-      </CodeBlock>
+    <div className={styles['wrapper']}>
+      <CodeBlock language="cURL">{exampleRequest}</CodeBlock>
       {!hideFooter && (
-        <div style={{marginTop: "-20px"}}>
+        <div className={styles['footer']}>
           {logged && (
-            <div className="code-terminal-footer">
-              {response && <pre style={{backgroundColor: "#292A35"}}>{response}</pre>}
+            <div className={clsx(styles['response'], 'type-paragraph-m')}>
+              {response && <pre style={{ backgroundColor: '#292A35' }}>{response}</pre>}
             </div>
           )}
-          <div className="code-terminal-footer bg-light-gray">
+          <div className={styles['buttons']}>
             {logged ? (
-              <div className="btn-row">
-                <button className="submit-btn" type="submit">
-                  <span>Send Request</span>
-                  <Icon name="arrow-right" />
-                </button>
-              </div>
+              <Button
+                as="button"
+                buttonType="submit"
+                label={'Send Request'}
+                icon={'arrow-right'}
+                style={
+                  theme === 'dark'
+                    ? {
+                        '--button-color-hover': 'var(--general-white)',
+                        '--button-text-color-hover': 'var(--general-black)',
+                      }
+                    : {
+                        '--button-color-hover': 'var(--general-black)',
+                        '--button-text-color-hover': 'var(--general-white)',
+                      }
+                }
+              />
             ) : (
-              <p className="description-row">
-                <Link target="_blank" to={`${API_URL}/login`} rel="noreferrer">
-                  Sign in
-                </Link>{" "}
-                or{" "}
-                <Link
+              <p className="type-paragraph-m">
+                <Button
+                  as="link"
                   target="_blank"
-                  to={`${API_URL}/register`}
-                  rel="noreferrer">
-                  create an account
-                </Link>{" "}
+                  href={`${API_URL}/login`}
+                  label={'Sign in'}
+                  style={
+                    theme === 'dark'
+                      ? {
+                          '--button-color-hover': 'var(--general-white)',
+                          '--button-text-color-hover': 'var(--general-black)',
+                        }
+                      : {
+                          '--button-color-hover': 'var(--general-black)',
+                          '--button-text-color-hover': 'var(--general-white)',
+                        }
+                  }
+                />{' '}
+                or{' '}
+                <Button
+                  as="link"
+                  target="_blank"
+                  href={`${API_URL}/register`}
+                  label={'Create an account'}
+                  style={
+                    theme === 'dark'
+                      ? {
+                          '--button-color-hover': 'var(--general-white)',
+                          '--button-text-color-hover': 'var(--general-black)',
+                        }
+                      : {
+                          '--button-color-hover': 'var(--general-black)',
+                          '--button-text-color-hover': 'var(--general-white)',
+                        }
+                  }
+                />{' '}
                 and reload the page to edit real requests
               </p>
             )}
@@ -65,7 +111,7 @@ const TerminalViewBox = ({
         </div>
       )}
     </div>
-  );
-};
+  )
+}
 
-export default TerminalViewBox;
+export default TerminalViewBox
