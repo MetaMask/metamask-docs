4acf9b0b9393c8e1beeb7292181349eacf0d76af
Aslan A <ak.fordev@gmail.com>
2024-08-20 15:50:01 +0500

    refactor(test page): Add CodeTerminal component and remove SnapsSection

--- src/pages/test.jsx
+++ src/pages/test.jsx
@@ -1,10 +1,10 @@
 import Layout from '@theme/Layout'
 import Heading from '@theme/Heading'
 import Pill from '@site/src/components/Pill'
-import SnapsSection from '@site/src/components/SnapsSection'
 import YoutubeEmbed from '@site/src/components/YoutubeEmbed'
 import SectionAPIs from '@site/src/components/Sections/SectionAPIs'
 import SectionNetworks from '@site/src/components/Sections/SectionNetworks'
+import CodeTerminal from '@site/src/components/CodeTerminal/CodeTerminal'
 
 export default function Test() {
   return (
@@ -45,41 +45,12 @@ export default function Test() {
           the user experience.
         </Pill>
 
-        <SnapsSection
-          items={[
-            {
-              colorPalette: 'purple',
-              title: 'Extend the functionality of MetaMask using Snaps',
-              description:
-                'A Snap is a JavaScript program run in an isolated environment that customizes the MetaMask wallet experience. You can create a Snap that adds new API methods, adds support for different blockchain protocols, or modifies existing functionalities.',
-              cards: [
-                {
-                  title: 'Snaps quickstart',
-                  description:
-                    'Get started quickly by creating and customizing a simple Snap, using the Snaps template built with React and TypeScript.',
-                  href: '/snaps/get-started/quickstart',
-                },
-                {
-                  title: 'Snaps tutorials',
-                  description:
-                    'Follow the step-by-step tutorials to create Snaps that estimate gas fees, provide transaction insights with custom UI, and more.',
-                  href: '/snaps/learn/tutorials',
-                },
-                {
-                  title: 'Snaps API',
-                  description:
-                    'Use the Snaps API to modify the functionality of MetaMask and communicate between dapps and Snaps.',
-                  href: '/snaps/learn/tutorials',
-                },
-              ],
-            },
-          ]}
-        />
-
         <YoutubeEmbed url="https://www.youtube.com/embed/qZRAryYwgdg?si=CeImIULgH3iD-FF0" />
 
         <SectionAPIs />
         <SectionNetworks />
+
+        <CodeTerminal />
       </div>
     </Layout>
   )
