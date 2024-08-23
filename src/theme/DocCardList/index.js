0a63c5e4e8c9b1dc571abd6438656e83f0c8d363
Aslan A <ak.fordev@gmail.com>
2024-08-23 15:16:12 +0500

    refactor(DocCardList): update structure and styles

--- src/theme/DocCardList/index.js
+++ src/theme/DocCardList/index.js
@@ -1,22 +1,69 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
 import clsx from 'clsx'
 import { useCurrentSidebarCategory, filterDocCardListItems } from '@docusaurus/theme-common'
+import { useColorMode } from '@docusaurus/theme-common'
 import DocCard from '@theme/DocCard'
+import CutOffCorners from '@site/src/components/elements/cut-off-corners'
+import Button from '@site/src/components/elements/buttons/button'
+
+import styles from './styles.module.scss'
 
 function DocCardListForCurrentSidebarCategory({ className }) {
   const category = useCurrentSidebarCategory()
   return <DocCardList items={category.items} className={className} />
 }
+
 export default function DocCardList(props) {
+  const { colorMode } = useColorMode()
+  const [theme, setTheme] = useState('')
   const { items, className } = props
+  const [activeIndex, setActiveIndex] = useState(null)
+
+  useEffect(() => {
+    setTheme(colorMode)
+  }, [colorMode])
+
   if (!items) {
     return <DocCardListForCurrentSidebarCategory {...props} />
   }
   const filteredItems = filterDocCardListItems(items)
   return (
-    <section className={clsx('row', className)}>
+    <section className={clsx(styles['row'], 'row', className)}>
       {filteredItems.map((item, index) => (
-        <DocCard key={index} item={item} />
+        <article
+          key={index}
+          className={clsx(styles['item'], activeIndex === index && styles['active'])}>
+          <CutOffCorners size={'s'}>
+            <div
+              className={styles['holder']}
+              onMouseEnter={() => setActiveIndex(index)}
+              onMouseLeave={() => setActiveIndex(null)}>
+              <DocCard item={item} />
+
+              {item.href && (
+                <Button
+                  as="link"
+                  href={item.href}
+                  label={false}
+                  type={theme === 'dark' ? 'secondary' : 'primary'}
+                  icon="arrow-right"
+                  className={styles['button']}
+                  style={
+                    theme === 'dark'
+                      ? {
+                          '--button-color-hover': 'var(--general-white)',
+                          '--button-text-color-hover': 'var(--general-black)',
+                        }
+                      : {
+                          '--button-color-hover': 'var(--general-black)',
+                          '--button-text-color-hover': 'var(--general-white)',
+                        }
+                  }
+                />
+              )}
+            </div>
+          </CutOffCorners>
+        </article>
       ))}
     </section>
   )
