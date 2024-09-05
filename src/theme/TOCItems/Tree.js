07922d3338788e488daec9c21516c220d73d21d8
Baptiste Briel <baptiste@antinomy.studio>
2024-09-05 13:58:18 +0200

    (fix) table of contents styling and responsive

--- src/theme/TOCItems/Tree.js
+++ src/theme/TOCItems/Tree.js
@@ -7,7 +7,7 @@ import styles from './styles.module.scss'
 export default function TreeWrapper(props) {
   return (
     <>
-      <span className={clsx(styles['subtitle'], 'type-label-eyebrow')}>Table of content:</span>
+      <span className={clsx(styles['subtitle'], 'type-label-eyebrow')}>On this page:</span>
       <Tree {...props} />
     </>
   )
