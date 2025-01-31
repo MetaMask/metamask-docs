23eebe1de08a5543a7a200cd837949ad5e5d96d4
Alexandra Tran <alexandratran@protonmail.com>
2025-01-31 12:14:23 -0800

    update homepage to 2x2 grid layout

--- src/components/CardSection.tsx
+++ src/components/CardSection.tsx
@@ -6,18 +6,12 @@ import Card, { CardItem } from '@site/src/components/Card'
 
 import styles from './CardSection.module.scss'
 
-type CardSectionItem = {
-  colorPalette: string
-  title: string
-  description: string
-  cards: CardItem[]
-}
-
 type CardSectionProps = {
-  items: CardSectionItem[]
+  cards: CardItem[]
+  colorPalette: string
 }
 
-export default function CardSection({ items }: CardSectionProps): JSX.Element {
+export default function CardSection({ cards, colorPalette }: CardSectionProps): JSX.Element {
   const { colorMode } = useColorMode()
   const [theme, setTheme] = useState('')
 
@@ -28,42 +22,31 @@ export default function CardSection({ items }: CardSectionProps): JSX.Element {
   return (
     <section className={styles['wrapper']}>
       <div className="container">
-        {items?.length > 0 &&
-          items.map(
-            ({ colorPalette, title, description, cards }: CardSectionItem, groupIndex: number) => (
-              <div
-                key={groupIndex}
-                className={clsx(styles['grid-wrapper'], groupIndex % 2 !== 0 && styles['reverse'])}
-                style={
-                  colorPalette
-                    ? ({
-                        '--color-palette': `var(--developer-${colorPalette})`,
-                      } as CSSProperties)
-                    : {}
-                }>
-                <div className={styles['grid-col-left']}>
-                  <div className={styles['sticky-col']}>
-                    <Heading as="h2" className={clsx(styles['title'], 'type-heading-m')}>
-                      {title}
-                    </Heading>
-                    <p className={clsx(styles['description'], 'type-paragraph-l')}>{description}</p>
-                  </div>
-                </div>
-                <ul className={styles['grid-col-right']}>
-                  {cards?.length > 0 &&
-                    cards.map(({ title, description, href }: CardItem, cardIndex: number) => (
-                      <Card
-                        key={cardIndex}
-                        title={title}
-                        description={description}
-                        href={href}
-                        theme={theme}
-                      />
-                    ))}
-                </ul>
-              </div>
-            )
-          )}
+        <div className={styles['grid-wrapper']}>
+          <div className={styles['grid-col-center']}>
+            <ul
+              className={styles['grid']}
+              style={
+                colorPalette
+                  ? ({
+                      '--color-palette': `var(--developer-${colorPalette})`,
+                    } as CSSProperties)
+                  : {}
+              }
+            >
+              {cards?.length > 0 &&
+                cards.map(({ title, description, href }: CardItem, cardIndex: number) => (
+                  <Card
+                    key={cardIndex}
+                    title={title}
+                    description={description}
+                    href={href}
+                    theme={theme}
+                  />
+                ))}
+            </ul>
+          </div>
+        </div>
       </div>
     </section>
   )
