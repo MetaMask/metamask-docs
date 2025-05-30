8993af2207dca8e1b24a9e705de4325679696f8a
bgravenorst <byron.gravenorst@consensys.net>
2025-05-30 13:43:13 +1000

    Add embedded wallets.
    Signed-off-by: bgravenorst <byron.gravenorst@consensys.net>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -412,6 +412,12 @@ const config = {
           },
         },
       },
+      announcementBar: {
+        id: 'support_us',
+        content:
+          '<span style="font-weight: 600">NEW!</span> Build embedded wallets with MetaMask using the <a target="_blank" rel="noopener noreferrer" href="https://web3auth.io/docs">Embedded Wallets SDK</a>. Instantly onboard users with social logins, passkeys, and more.',
+        isCloseable: false,
+      },
     }),
 }
 
