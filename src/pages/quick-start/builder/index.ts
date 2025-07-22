961d05eee30ceca1165ae050b5f804a126d5eb81
Yashovardhan Agrawal <21066442+yashovardhan@users.noreply.github.com>
2025-07-22 16:28:10 +0400

    Add MM SDK & DTK products in quick start

--- src/pages/quick-start/builder/index.ts
+++ src/pages/quick-start/builder/index.ts
@@ -1,40 +1,78 @@
 /* eslint-disable camelcase */
 import { IntegrationBuilder, IntegrationStep } from '../interfaces'
-import { quickStartHostedLinks, quickStartSourceCode } from '../../../utils/IBmaps'
-import { LANGS } from './choices'
+import {
+  DTKQuickStartSourceCode,
+  DTKQuickStartHostedLinks,
+  EWQuickStartHostedLinks,
+  EWQuickStartSourceCode,
+  MMSDKQuickStartSourceCode,
+  MMSDKQuickStartHostedLinks,
+} from '../../../utils/IBmaps'
+import {
+  PRODUCTS,
+  LANGS_EMBEDDED_WALLETS,
+  LANGS_METAMASK_SDK,
+  LANGS_DELEGATION_TOOLKIT,
+  EMBEDDED_WALLETS,
+  DELEGATION_TOOLKIT,
+} from './choices'
 
 import highlight from './highlight'
-import android from './android'
-import flutter from './flutter'
-import ios from './ios'
-import react_native from './react_native'
-import angular from './web/angular'
-import nextjs from './web/nextjs'
-import react from './web/react'
-import vue from './web/vue'
+import EWAndroid from './embedded-wallets/android'
+import EWFlutter from './embedded-wallets/flutter'
+import EWIos from './embedded-wallets/ios'
+import EWReactNative from './embedded-wallets/react_native'
+import EWAngular from './embedded-wallets/angular'
+import EWNextjs from './embedded-wallets/nextjs'
+import EWReact from './embedded-wallets/react'
+import EWVue from './embedded-wallets/vue'
+import MMReact from './metamask-sdk/react'
+import MMNextjs from './metamask-sdk/nextjs'
+import DTKReact from './delegation-toolkit/react'
+import DTKNextjs from './delegation-toolkit/nextjs'
 
 const frameworks = {
-  ANGULAR: angular,
-  NEXTJS: nextjs,
-  REACT: react,
-  VUE: vue,
-  ANDROID: android,
-  IOS: ios,
-  REACT_NATIVE: react_native,
-  FLUTTER: flutter,
+  EW_ANGULAR: EWAngular,
+  EW_NEXTJS: EWNextjs,
+  EW_REACT: EWReact,
+  EW_VUE: EWVue,
+  EW_ANDROID: EWAndroid,
+  EW_IOS: EWIos,
+  EW_REACT_NATIVE: EWReactNative,
+  EW_FLUTTER: EWFlutter,
+  MMSDK_REACT: MMReact,
+  MMSDK_NEXTJS: MMNextjs,
+  DTK_REACT: DTKReact,
+  DTK_NEXTJS: DTKNextjs,
+}
+
+// Helper function to map framework choice to framework key based on product
+const getFrameworkKey = (product: string, framework: string): string => {
+  if (product === EMBEDDED_WALLETS) {
+    return `EW_${framework}`
+  } else if (product === DELEGATION_TOOLKIT) {
+    return `DTK_${framework}`
+  } else {
+    return `MMSDK_${framework}`
+  }
 }
 
 const builder: IntegrationBuilder = {
   // Name of the integration builder
   displayName: 'Web3Auth',
-
   // Options that will be displayed in the UI for selection
   options: {
+    product: {
+      displayName: 'Product',
+      default: PRODUCTS[0].key,
+      type: 'product_selection',
+      choices: PRODUCTS,
+    },
     framework: {
       displayName: 'Platform/ Framework',
-      default: LANGS[0].key,
+      default: LANGS_EMBEDDED_WALLETS[0].key,
       type: 'dropdown',
-      choices: LANGS,
+      choices: LANGS_EMBEDDED_WALLETS,
     },
   },
 
@@ -46,24 +84,60 @@ const builder: IntegrationBuilder = {
     const newFiles = JSON.parse(JSON.stringify(files))
     const steps: IntegrationStep[] = []
 
-    let frameworkDefault
+    let frameworkChoices, frameworkDefault
 
-    frameworkDefault = LANGS[0].key
+    if (finalValues.product === EMBEDDED_WALLETS) {
+      frameworkChoices = LANGS_EMBEDDED_WALLETS
+      frameworkDefault = LANGS_EMBEDDED_WALLETS[0].key
+    } else if (finalValues.product === DELEGATION_TOOLKIT) {
+      frameworkChoices = LANGS_DELEGATION_TOOLKIT
+      frameworkDefault = LANGS_DELEGATION_TOOLKIT[0].key
+    } else {
+      frameworkChoices = LANGS_METAMASK_SDK
+      frameworkDefault = LANGS_METAMASK_SDK[0].key
+    }
 
-    if (!LANGS.map(item => item.key).includes(finalValues.framework)) {
+    if (!frameworkChoices.map(item => item.key).includes(finalValues.framework)) {
       finalValues.framework = frameworkDefault
     }
 
     this.options = {
+      product: {
+        displayName: 'Product',
+        default: PRODUCTS[0].key,
+        type: 'product_selection',
+        choices: PRODUCTS,
+      },
       framework: {
         displayName: 'Platform/ Framework',
         default: frameworkDefault,
         type: 'dropdown',
-        choices: LANGS,
+        choices: frameworkChoices,
       },
     }
 
-    frameworks[finalValues.framework].build({ ...finalValues, filenames, files: newFiles, steps })
+    let sourceCodeLink, embedLink
+
+    if (finalValues.product === EMBEDDED_WALLETS) {
+      sourceCodeLink = EWQuickStartSourceCode[finalValues.framework]
+      embedLink = EWQuickStartHostedLinks[finalValues.framework]
+    } else if (finalValues.product === DELEGATION_TOOLKIT) {
+      sourceCodeLink = DTKQuickStartSourceCode[finalValues.framework]
+      embedLink = DTKQuickStartHostedLinks[finalValues.framework]
+    } else {
+      sourceCodeLink = MMSDKQuickStartSourceCode[finalValues.framework]
+      embedLink = MMSDKQuickStartHostedLinks[finalValues.framework]
+    }
+
+    // Get the correct framework key by mapping the choice to the framework object key
+    const frameworkKey = getFrameworkKey(finalValues.product, finalValues.framework)
+
+    // Check if the framework exists before calling build
+    if (!frameworks[frameworkKey]) {
+      throw new Error(`Framework not found: ${frameworkKey}`)
+    }
+
+    frameworks[frameworkKey].build({ ...finalValues, filenames, files: newFiles, steps })
 
     if (stepIndex >= steps.length) {
       // eslint-disable-next-line no-param-reassign
@@ -80,8 +154,8 @@ const builder: IntegrationBuilder = {
         pointer: it.pointer ? { ...it.pointer, filename: `${it.pointer.filename}` } : undefined,
       })),
       stepIndex,
-      embedLink: quickStartHostedLinks[finalValues.framework],
-      sourceCodeLink: quickStartSourceCode[finalValues.framework],
+      embedLink,
+      sourceCodeLink,
     }
   },
 }
