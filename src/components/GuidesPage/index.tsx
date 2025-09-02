41c592175caa7d790ba696658ffeba7ea0b3b060
Yashovardhan Agrawal <21066442+yashovardhan@users.noreply.github.com>
2025-09-02 23:39:02 +0400

    Fix SEO component

--- src/components/GuidesPage/index.tsx
+++ src/components/GuidesPage/index.tsx
@@ -143,7 +143,7 @@ export default function Guides({ content = {} }: GuidesInterface) {
       <SEO
         title="Tutorials"
         description="Tutorials to integrate, customize, and build with MetaMask developer tools."
-        image={`${baseUrl}images/tutorialsog.jpg`}
+        image={`${baseUrl}img/tutorialsog.jpg`}
         slug="/guides"
       />
 
