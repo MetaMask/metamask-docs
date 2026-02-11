6a606dfbbea9d0e7dfa6bf5486993249f3f1f345
Andy Bridges <andymbridges@gmail.com>
2026-02-11 19:05:52 +0000

    move osano script to headTags

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -35,6 +35,12 @@ const config = {
 
   headTags: [
     {
+      tagName: 'script',
+      attributes: {
+        src: 'https://cmp.osano.com/AzZMxHTbQDOQD8c1J/84e64bce-4a70-4dcc-85cb-7958f22b2371/osano.js',
+      },
+    },
+    {
       tagName: 'link',
       attributes: {
         rel: 'icon',
@@ -129,9 +135,6 @@ const config = {
       src: baseUrl + 'js/code-focus.js',
       async: false,
       defer: true,
-    },
-    {
-      src: 'https://cmp.osano.com/AzZMxHTbQDOQD8c1J/84e64bce-4a70-4dcc-85cb-7958f22b2371/osano.js',
     },
   ],
 
