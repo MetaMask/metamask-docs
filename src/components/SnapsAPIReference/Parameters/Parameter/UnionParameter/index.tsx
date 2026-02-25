4d291c035a3d5602f4170a0b113c1cd7bc557748
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2026-02-25 14:32:36 +0100

    Add support for complex types

---
+++ src/components/SnapsAPIReference/Parameters/Parameter/UnionParameter/index.tsx
@@ -0,0 +1,52 @@
+import { FunctionComponent } from 'react'
+import { UnionMethodParameter } from '../types'
+import styles from '../styles.module.scss'
+import { Parameter } from '../index'
+import { Header } from '../Header'
+import { Description } from '../Description'
+import classNames from 'classnames'
+
+export type UnionParameterProps = {
+  parameter: UnionMethodParameter
+  nested?: boolean
+}
+
+export const UnionParameter: FunctionComponent<UnionParameterProps> = ({ parameter, nested }) => {
+  const { description, options } = parameter
+
+  return (
+    <div className={nested ? styles['parameter-child-item'] : styles.parameter}>
+      <div className={styles['parameter-main']}>
+        <Header parameter={parameter} type="union" />
+        {description && <Description>{description}</Description>}
+      </div>
+
+      <h3 className="margin-vert--lg type-paragraph-s">Options</h3>
+      <div className={styles['parameter-children']}>
+        {Object.entries(options).map(([key, property], index, array) => (
+          <>
+            <div key={`${key}-${property.name}`} className={styles['parameter-child']}>
+              <Parameter parameter={property} nested={true} />
+            </div>
+            {index < array.length - 1 && (
+              <p className={classNames('type-paragraph-s', styles['parameter-separator'])}>or</p>
+            )}
+          </>
+        ))}
+      </div>
+
+      {parameter.commonProperties?.length > 0 && (
+        <>
+          <h3 className="margin-vert--lg type-paragraph-s">Common properties</h3>
+          <div className={styles['parameter-children']}>
+            {Object.entries(parameter.commonProperties).map(([key, property]) => (
+              <div key={`${key}-${property.name}`} className={styles['parameter-child']}>
+                <Parameter parameter={property} nested={true} />
+              </div>
+            ))}
+          </div>
+        </>
+      )}
+    </div>
+  )
+}
---
+++ src/components/SnapsAPIReference/Parameters/Parameter/types.ts
@@ -0,0 +1,31 @@
+type CommonMethodParameter = {
+  type: string;
+  name?: string;
+  description?: string | null;
+  required?: boolean;
+};
+export type ObjectMethodParameter = CommonMethodParameter & {
+  kind: 'object';
+  properties: MethodParameter[];
+};
+
+export type PrimitiveMethodParameter = CommonMethodParameter & {
+  kind: 'primitive';
+};
+
+export type ArrayMethodParameter = CommonMethodParameter & {
+  kind: 'array';
+  element: MethodParameter;
+};
+
+export type UnionMethodParameter = CommonMethodParameter & {
+  kind: 'union';
+  commonProperties: MethodParameter[];
+  options: MethodParameter[];
+};
+
+export type MethodParameter =
+  | ObjectMethodParameter
+  | PrimitiveMethodParameter
+  | ArrayMethodParameter
+  | UnionMethodParameter;
