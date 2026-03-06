d7be5e476aa2b7749b2aaf779052e0190f0267f2
bgravenorst <byron.gravenorst@consensys.net>
2026-03-06 10:44:06 +1000

    Add banner.
    Signed-off-by: bgravenorst <byron.gravenorst@consensys.net>

--- src/theme/AnnouncementBar/index.tsx
+++ src/theme/AnnouncementBar/index.tsx
@@ -7,7 +7,7 @@ export default function AnnouncementBar(): JSX.Element | null {
   return (
     <div className="theme-announcement-bar" role="banner" data-announcement-bar="true">
       <div style={{ textAlign: 'center' }}>
-        <strong>MetaMask SDK</strong> has been replaced by <strong>MetaMask Connect</strong>. One integration for EVM, Solana, and more networks coming soon. <a href="/sdk">Learn more</a>.
+        <strong style={{ fontWeight: 700 }}>MetaMask SDK</strong> has been replaced by <strong style={{ fontWeight: 700 }}>MetaMask Connect</strong>. One integration for EVM, Solana, and more networks coming soon. <a href="/sdk">Learn more</a>.
       </div>
     </div>
   )
